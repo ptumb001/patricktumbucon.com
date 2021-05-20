9c17d7b4ba6447e26c0b32eb26bd58b7ecda4a29
Patrick Tumbucon <patricktumbucon@gmail.com>
2021-05-20 10:27:30 -0700

    Update About section - current job

--- pages/about.vue
+++ pages/about.vue
@@ -4,15 +4,16 @@
     <section>
       <h2>What I've been up to</h2>
       <p>
-        I'm a Software Development Engineer at <a href="http://amazon.com">Amazon</a>, based in Seattle, Washington. I work in  
-        <a href="https://amazon.jobs/en/business_categories/customer-trust-and-partner-support">CTPS</a>, where we utilize machine learning to prevent seller fraud before it can happen. I graduated from  
-        <a href="http://ucr.edu">UC Riverside</a> with a BS in Computer Engineering.
+        I'm a Software Engineer at <a href="https://microsoft.com">Microsoft</a>, based in Redmond, Washington. I work on ML/AI insights for  
+        <a href="https://docs.microsoft.com/en-us/azure/active-directory/governance/identity-governance-overview">Azure Active Directory Identity Governance</a>. I attended
+        <a href="https://ucr.edu">UC Riverside</a>, pursuing a BS in Computer Engineering.
       </p>
       <p>
-        Previously, I've increased search presence at
-        <a href="http://esri.com">Esri</a>, professionally developed students at 
-        <a href="http://acmucr.org">ACM@UCR</a>, and organized multiple events at 
-        <a href="http://citrushack.com">Citrus Hack</a>.
+        Previously, I've built upon fraud-predicting ML systems at 
+        <a href="https://amazon.com">Amazon</a>, increased search presence at
+        <a href="https://esri.com">Esri</a>, professionally developed students at 
+        <a href="https://acmucr.org">ACM@UCR</a>, and organized multiple events at 
+        <a href="https://citrushack.com">Citrus Hack</a>.
       </p>
     </section>
     
